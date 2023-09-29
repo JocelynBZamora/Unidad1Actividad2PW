978e704a2e83d97f80f088f7ad31555d373ae53a
JocelynBZamora <201g0293@rcarbonifera.tecnm.mx>
2023-09-29 13:35:46 -0600

    Agregar archivos de proyecto.

---
+++ Unidad1Actividad2PW/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+
+namespace Unidad1Actividad2PW.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int valor, string Moneda)
+        {
+            int Resultado = 0;
+            if (Moneda == "USD")
+            {
+                Resultado = valor / 18;
+                return View(Resultado);
+            }
+            else
+            {
+                Resultado = valor * 18;
+                return View(Resultado);
+            }
+
+
+        }
+    }
+}
